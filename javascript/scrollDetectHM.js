89719393c1fb9ede88082ead8a217c04dfeff84d
NickGByrne <132789498+NickGByrne@users.noreply.github.com>
2026-04-12 23:37:34 -0700

    upload

---
+++ javascript/scrollDetectHM.js
@@ -0,0 +1,18 @@
+window.onscroll = function() {myFunction()};
+
+	function myFunction(){
+
+		 if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350){
+			document.getElementById("onScroll").style.display="block";
+			
+		} else {
+			document.getElementById("onScroll").style.display="none";
+		}
+	}
+			
+
+	
+
+
+ 
+	
